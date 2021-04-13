0675cee3716489e19312a09b5144d3ca8b6db348
Atomini <62155406+Atomini@users.noreply.github.com>
2021-04-13 20:00:06 +0300

    start flafy

--- parser_bot/parser_bot/spiders/flafy.py
+++ parser_bot/parser_bot/spiders/flafy.py
@@ -1,4 +1,6 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
+
 
 
 class FlafySpider(scrapy.Spider):
@@ -6,9 +8,8 @@ class FlafySpider(scrapy.Spider):
     start_urls = ['https://flatfy.ua/search?currency=UAH&geo_id=27&price_max=650000&section_id=1']
 
     def parse(self, response):
-        last_page = response.xpath('//*[@id="root"]/div/div[2]/div/div[2]/div/div[3]/div[1]/a[4]').extract()
-
-        if int(last_page) > 3:
+        last_page = response.xpath('//*[@class="paging-button"]/text()').extract()
+        if int(last_page[-1]) > 3:
             pages: int = 3
         else:
             pages: int = int(last_page)
@@ -20,6 +21,16 @@ class FlafySpider(scrapy.Spider):
     def parse_page(self, response, **kwargs):
         link = response.xpath('//*[@rel="nofollow noopener"]/@href').extract()
         title = response.xpath('//*[@rel="nofollow noopener"]/text()').extract()
-        district = response.xpath('//*[@class="realty-preview__sub-title"]/text()').extract()
+        district = response.xpath('//*[@class="realty-preview__sub-title"]/a[1]/text()').extract()
+        # '//*[@id="380231826"]/div[1]/div[1]/div/div[1]/div[1]/div[2]/a[1]/text()'
+        print(link)
+        print(title)
+        print(district)
+        print(len(link), "-",len(title), "-",len(district))
 
-        'realty-preview__sub-title'
+if __name__ == "__main__":
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    })
+    process.crawl(FlafySpider)
+    process.start()  # the script will block here until the crawling is finished
