060d7e7e57d511248aa179717009bc66a69a3c0a
Atomini <62155406+Atomini@users.noreply.github.com>
2021-04-18 11:12:18 +0300

    fix 1

--- hendlers/admin/startup_message.py
+++ hendlers/admin/startup_message.py
@@ -1,8 +1,9 @@
 import os
 from misc import bot
+from misc import dp
 
 admin_id = os.getenv("admin_id")
 
 
-async def send_start_message():
+async def send_start_message(dp):
     await bot.send_message(chat_id=admin_id, text="Бот запущен /start")
